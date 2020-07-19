2e59f53630bf3647f84e3db383a71c33cfe153cd
Kanav Bengani <kbengani21@gmail.com>
2020-07-18 23:55:58 -0500

    Lifeguard Passcode

--- lib/screens/profiles/lifeguard.dart
+++ lib/screens/profiles/lifeguard.dart
@@ -13,6 +13,8 @@ class Lifeguard extends StatefulWidget {
 
 class _LifeguardState extends State<Lifeguard> {
   int _currentIndex = 0;
+  String passcode = '';
+  final _formKey = GlobalKey<FormState>();
 
   final tabs = [
     All(),
@@ -23,64 +25,93 @@ class _LifeguardState extends State<Lifeguard> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.lightBlue[50],
-      appBar: AppBar(
+    if (passcode == '1234') {
+      return Scaffold(
+        backgroundColor: Colors.lightBlue[50],
+        appBar: AppBar(
+            backgroundColor: Colors.lightBlue[100],
+            elevation: 0.0,
+            title: Text('Lifeguard')),
+        body: tabs[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.all_inclusive,
+                color: Colors.black,
+              ),
+              title: Text(
+                'All',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.trending_up,
+                color: Colors.black,
+              ),
+              title: Text(
+                'High',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.trending_flat,
+                color: Colors.black,
+              ),
+              title: Text(
+                'Medium',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.trending_down,
+                color: Colors.black,
+              ),
+              title: Text(
+                'Low',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
           backgroundColor: Colors.lightBlue[100],
-          elevation: 0.0,
-          title: Text('Lifeguard')),
-      body: tabs[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.all_inclusive,
-              color: Colors.black,
-            ),
-            title: Text(
-              'All',
-              style: TextStyle(color: Colors.black),
-            ),
+          title: Text('Lifeguard Passcode'),
+        ),
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Form(
+            key: _formKey,
+            child: Column(children: <Widget>[
+              SizedBox(height: 20.0),
+              TextFormField(
+                validator: (val) => val.isEmpty ? 'Incorrect Passcode' : null,
+                decoration: InputDecoration(
+                  hintText: 'Enter Passcode to Access Data',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() => passcode = val);
+                },
+              ),
+            ]),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.trending_up,
-              color: Colors.black,
-            ),
-            title: Text(
-              'High',
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.trending_flat,
-              color: Colors.black,
-            ),
-            title: Text(
-              'Medium',
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.trending_down,
-              color: Colors.black,
-            ),
-            title: Text(
-              'Low',
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-        ],
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-      ),
-    );
+        ),
+      );
+    }
   }
 }
 
